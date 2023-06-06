b2fa440956a70c2a9bfb00447140eee2fc116e4e
Matthieu <Matthieu.Maginot@gmail.com>
2023-06-06 09:50:39 +0200

    commit

---
+++ src/app/cocktail.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Cocktail } from './models/cocktail.model';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CocktailService {
+
+  constructor(public http: HttpClient) { }
+
+  getCocktails(): Observable<Cocktail[]> {
+    return this.http.get<Cocktail[]>("assets/cocktails.json");
+  }
+}
---
+++ src/app/models/cocktail.model.ts
@@ -0,0 +1,11 @@
+export class Cocktail {
+    nom: string;
+    prix: number;
+    image: string;
+
+    constructor() {
+        this.nom = "";
+        this.prix = 0;
+        this.image = "";
+    }
+}
